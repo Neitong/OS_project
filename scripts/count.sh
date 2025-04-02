0219a62b43a6bca0ff636e46e8e5922e2a8cf165
RaFat <manrafat2006@gmail.com>
2025-04-03 00:17:43 +0700

    update

--- scripts/count.sh
+++ scripts/count.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+source "$(dirname "$0")/../log.sh"
 
 read -p "Enter the directory path: " dir
 
 if [ -d "$dir" ]; then
     file_count=$(find "$dir" -type f | wc -l)
     echo "Total files in $dir: $file_count"
+    log_action "Counted $file_count file(s) in directory '$dir'."
 else
     echo "Error: Directory does not exist."
+    log_action "Count failed: Directory '$dir' does not exist."
 fi
