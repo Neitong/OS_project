83b8ca99fd08c998cbcd7729246cfac7fe9a0eda
RaFat <manrafat2006@gmail.com>
2025-04-03 00:20:30 +0700

    update

--- scripts/Disk_Usage.sh
+++ scripts/Disk_Usage.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+source "$(dirname "$0")/../log.sh"
 
 # Display disk usage of a directory
 read -p "Enter directory path: " dir
 if [ -d "$dir" ]; then
     du -sh "$dir"
     echo "Displayed disk usage for $dir" >> script.log
+    log_action "Displayed disk usage for $dir"
 else
     echo "Invalid directory."
+    log_action "Invalid directory."
 fi
